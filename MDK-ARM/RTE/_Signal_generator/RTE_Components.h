479353e7ef9105f6a95e481cfe4a0740d2d37f88
igorberaldo <beraldo_igor@hotmail.com>
2020-05-21 14:41:38 -0300

    Add files via upload

---
+++ MDK-ARM/RTE/_Signal_generator/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Signal generator' 
+ * Target:  'Signal generator' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/Signal generator/startup_stm32l053xx.d
@@ -0,0 +1,1 @@
+signal generator\startup_stm32l053xx.o: startup_stm32l053xx.s
